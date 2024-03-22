f13c2a25bf65c99fdfd5a2993e7743f330ad14e9
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2024-03-22 17:52:51 +0100

    Working on Onboarding1 Screen

--- Screens/Onboarding/Onboarding1.js
+++ Screens/Onboarding/Onboarding1.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, Dimensions } from "react-native";
+import React from "react";
+
+const { width, height } = Dimensions.get("window");
 
 const Onboarding1 = () => {
   return (
-    <View>
-      <Text>Onboarding1</Text>
+    <View style={{ padding: 25 }}>
+      <View
+        style={{
+          width: width,
+          height: height * 0.4,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingRight: 25
+        }}
+      >
+        <Image
+          source={require("./../../assets/Onboarding/currency_exchange.png")}
+          style={{ width: "70%", height: "70%" }}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Onboarding1
+export default Onboarding1;
