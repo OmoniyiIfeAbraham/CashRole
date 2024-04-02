993371ad2395a7e8f64ac7a0bdeeaaad90f2da3c
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2024-04-02 04:29:00 +0100

    Update

---
+++ Components/HomeScreen/WithdrawComponent.js
@@ -0,0 +1,46 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import Colors from "../../Style/ThemeColors";
+import GeneralStyle from "../../Style/General.style";
+
+const WithdrawComponent = () => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: 300,
+        backgroundColor: Colors.white,
+        elevation: 20,
+        marginTop: 10,
+        borderRadius: 5,
+        shadowColor: Colors.black,
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={[GeneralStyle.RegularText, { color: Colors.black }]}>
+        Available funds
+      </Text>
+      <Text
+        style={[GeneralStyle.BoldText, { color: Colors.black, fontSize: 30 }]}
+      >
+        0.00
+      </Text>
+      <View style={{ width: "100%", paddingHorizontal: 50, marginTop: 25 }}>
+        <TouchableOpacity
+          style={[GeneralStyle.Btn, { backgroundColor: Colors.midnightBlue }]}
+        >
+          <Text style={[GeneralStyle.BoldText]}>Withdraw</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default WithdrawComponent;
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,9 +1,14 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  AntDesign,
+  FontAwesome5,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import Colors from "../Style/ThemeColors";
 import GeneralStyle from "../Style/General.style";
+import WithdrawComponent from "../Components/HomeScreen/WithdrawComponent";
 
 const Home = () => {
   return (
@@ -21,7 +26,7 @@ const Home = () => {
           <View style={{ flexDirection: "row", alignItems: "center" }}>
             <View
               style={{
-                padding: 25,
+                padding: 15,
                 backgroundColor: Colors.ash,
                 borderRadius: 50,
                 marginRight: 10,
@@ -36,11 +41,35 @@ const Home = () => {
           <View style={{ justifyContent: "center", alignItems: "center" }}>
             <MaterialCommunityIcons
               name="bell"
-              size={40}
+              size={35}
               color={Colors.midnightBlue}
             />
           </View>
         </View>
+        {/* balance */}
+        <View style={{ marginTop: 30 }}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text
+              style={[
+                GeneralStyle.RegularText,
+                { color: Colors.black, marginRight: 20 },
+              ]}
+            >
+              Total Balance
+            </Text>
+            <AntDesign name="exclamationcircleo" size={24} color="black" />
+          </View>
+          <Text
+            style={[
+              GeneralStyle.BoldText,
+              { color: Colors.black, fontSize: 30 },
+            ]}
+          >
+            #100,000.00
+          </Text>
+        </View>
+        {/* withdraw */}
+        <WithdrawComponent />
       </ScrollView>
     </SafeAreaView>
   );
