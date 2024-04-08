e601cd1ca4b8acd4ca8ebf344e06ca78cb188b76
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2024-04-08 14:55:55 +0100

    Update on Withdraw Screen

--- Screens/Sellers Screens/Withdraw.js
+++ Screens/Sellers Screens/Withdraw.js
@@ -1,11 +1,46 @@
-import { Keyboard, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Keyboard,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { MaterialIcons } from "@expo/vector-icons";
 import Colors from "../../Style/ThemeColors";
 import GeneralStyle from "../../Style/General.style";
+import RNPickerSelect from "react-native-picker-select";
+import { useState } from "react";
+
+const { width, height } = Dimensions.get("window");
 
 const Withdraw = ({ navigation }) => {
+  const [selectedValue, setSelectedValue] = useState(null);
+
+  const pickerSelectStyles = StyleSheet.create({
+    inputAndroid: [
+      GeneralStyle.TextInput,
+      {
+        fontWeight: "500",
+        paddingRight: 30, // to ensure the text is never behind the icon
+        marginTop: 0,
+        height: "auto",
+        color: Colors.black,
+        width: width * 0.9,
+      },
+    ],
+    placeholder: {
+      color: Colors.ash,
+    },
+  });
+
+  const data = [
+    { label: "Male", value: "Male" },
+    { label: "Female", value: "Female" },
+    { label: "Other", value: "Other" },
+  ];
   return (
     <SafeAreaView style={{ flex: 1, paddingHorizontal: 15 }}>
       {/* keyboard dismiss */}
@@ -57,6 +92,18 @@ const Withdraw = ({ navigation }) => {
         {/* form */}
         {/* bank */}
         <View style={GeneralStyle.TextInputView}>
+          <RNPickerSelect
+            onValueChange={(value) => setSelectedValue(value)}
+            items={data}
+            value={selectedValue}
+            placeholder={{ label: "Select a bank", value: null }}
+            style={{
+              ...pickerSelectStyles,
+            }}
+            Icon={() => {
+              return <View></View>;
+            }}
+          />
         </View>
       </Pressable>
     </SafeAreaView>
