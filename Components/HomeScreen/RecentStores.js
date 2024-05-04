4b71be9a54e5efe58cbdcc26b028dc7bc5400686
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2024-05-04 02:30:44 +0100

    Update On Stores Screen

--- Components/HomeScreen/RecentStores.js
+++ Components/HomeScreen/RecentStores.js
@@ -9,129 +9,17 @@ const RecentStores = () => {
     {
       id: 1,
       name: "Glamour Boutique",
-      desc: "Fashion Galore",
-      phone: "+2349022333991",
+      address: "Iwo road, Ibadan",
     },
     {
       id: 2,
-      name: "Tech Innovators",
-      desc: "Cutting-edge Gadgets",
-      phone: "+2349022333992",
+      name: "Glamour Boutique",
+      address: "Alagbaka, Akure",
     },
     {
       id: 3,
-      name: "Gourmet Delights",
-      desc: "Fine Dining Experience",
-      phone: "+2349022333993",
-    },
-    {
-      id: 4,
-      name: "Sporting Haven",
-      desc: "Fitness and Fun",
-      phone: "+2349022333994",
-    },
-    {
-      id: 5,
-      name: "Home Essentials",
-      desc: "Your Home Needs",
-      phone: "+2349022333995",
-    },
-    {
-      id: 6,
-      name: "Beauty Bliss",
-      desc: "Look Stunning",
-      phone: "+2349022333996",
-    },
-    {
-      id: 7,
-      name: "Bookworm's Paradise",
-      desc: "Reading Wonderland",
-      phone: "+2349022333997",
-    },
-    {
-      id: 8,
-      name: "Pet Kingdom",
-      desc: "For Your Furry Friends",
-      phone: "+2349022333998",
-    },
-    {
-      id: 9,
-      name: "Jewelry Galore",
-      desc: "Shine Bright",
-      phone: "+2349022333999",
-    },
-    { id: 10, name: "Toyland", desc: "Fun for Kids", phone: "+2349022333910" },
-    {
-      id: 11,
-      name: "Healthy Bites",
-      desc: "Nutritious Treats",
-      phone: "+2349022333911",
-    },
-    {
-      id: 12,
-      name: "Outdoor Adventure",
-      desc: "Explore Nature",
-      phone: "+2349022333912",
-    },
-    {
-      id: 13,
-      name: "Artisan Crafts",
-      desc: "Handmade Creations",
-      phone: "+2349022333913",
-    },
-    {
-      id: 14,
-      name: "Music Masters",
-      desc: "Melodies Galore",
-      phone: "+2349022333914",
-    },
-    {
-      id: 15,
-      name: "Wellness Oasis",
-      desc: "Health and Relaxation",
-      phone: "+2349022333915",
-    },
-    {
-      id: 16,
-      name: "Vintage Treasures",
-      desc: "Timeless Finds",
-      phone: "+2349022333916",
-    },
-    {
-      id: 17,
-      name: "Game Zone",
-      desc: "Playful Entertainment",
-      phone: "+2349022333917",
-    },
-    {
-      id: 18,
-      name: "Adventure Gear",
-      desc: "Gear up for Excitement",
-      phone: "+2349022333918",
-    },
-    {
-      id: 19,
-      name: "Green Thumb",
-      desc: "Gardening Delights",
-      phone: "+2349022333919",
-    },
-    {
-      id: 20,
-      name: "Cosmic Creations",
-      desc: "Universe-inspired Art",
-      phone: "+2349022333920",
-    },
-    {
-      id: 21,
-      name: "Comfort Haven",
-      desc: "Relaxation Essentials",
-      phone: "+2349022333921",
-    },
-    {
-      id: 22,
-      name: "Fashion Forward",
-      desc: "Style Redefined",
-      phone: "+2349022333922",
+      name: "Glamour Boutique",
+      address: "Abuja",
     },
   ];
 
@@ -141,39 +29,44 @@ const RecentStores = () => {
       style={{
         width: "100%",
         backgroundColor: Colors.white,
-        elevation: 1,
+        elevation: 5,
         flexDirection: "row",
-        paddingVertical: 10,
+        paddingVertical: 2,
         justifyContent: "space-around",
         alignItems: "center",
         marginBottom: 20,
+        height: 64,
+        borderRadius: 10,
       }}
     >
-      <View
-        style={{ backgroundColor: Colors.ash, borderRadius: 15, padding: 25 }}
-      >
-        <FontAwesome5 name="store-alt" size={36} color={Colors.black} />
-      </View>
-      <View style={{ justifyContent: "center" }}>
-        <Text style={[GeneralStyle.BoldText, { color: Colors.black }]}>
-          {item.name}
-        </Text>
-        <Text
-          style={[
-            GeneralStyle.LightText,
-            { color: Colors.black, fontSize: 20 },
-          ]}
+      <View style={{ flexDirection: "row", width: "auto" }}>
+        {/* icon */}
+        <View
+          style={{
+            backgroundColor: Colors.ash,
+            borderRadius: 10,
+            padding: 10,
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: 10,
+          }}
         >
-          {item.desc}
-        </Text>
-        <Text
-          style={[
-            GeneralStyle.MediumText,
-            { color: Colors.black, fontSize: 22 },
-          ]}
-        >
-          {item.phone}
-        </Text>
+          <FontAwesome5 name="store-alt" size={18} color={Colors.black} />
+        </View>
+        {/* name and address */}
+        <View style={{ justifyContent: "flex-start" }}>
+          <Text style={[GeneralStyle.BoldText, { color: Colors.black }]}>
+            {item.name}
+          </Text>
+          <Text
+            style={[
+              GeneralStyle.LightText,
+              { color: Colors.black, fontSize: 20 },
+            ]}
+          >
+            {item.address}
+          </Text>
+        </View>
       </View>
       <View
         style={{
@@ -191,7 +84,7 @@ const RecentStores = () => {
     </TouchableOpacity>
   ));
   return (
-    <View style={{ paddingVertical: 5, flex: 1 }}>
+    <View style={{ paddingVertical: 5, flex: 1, paddingHorizontal: 25 }}>
       <FlatList
         showsVerticalScrollIndicator={false}
         data={stores}
--- Screens/Sellers Screens/Stores.js
+++ Screens/Sellers Screens/Stores.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
@@ -11,8 +11,8 @@ const Stores = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1, paddingHorizontal: 15 }}>
       {/* header */}
-      <Header navigation={navigation} title="Stores" />
-      <View style={{ marginTop: 15, alignItems: "center", marginBottom: 15 }}>
+      <Header navigation={navigation} title="Your Stores" />
+      {/* <View style={{ marginTop: 15, alignItems: "center", marginBottom: 15 }}>
         <View
           style={{
             flexDirection: "row",
@@ -44,6 +44,17 @@ const Stores = ({ navigation }) => {
         >
           Your Stores
         </Text>
+      </View> */}
+      {/* btn */}
+      <View
+        style={{ width: "100%", paddingHorizontal: 50, marginVertical: 20 }}
+      >
+        <TouchableOpacity
+          style={[GeneralStyle.Btn, { backgroundColor: Colors.midnightBlue }]}
+          onPress={() => navigation.navigate("AddStore")}
+        >
+          <Text style={[GeneralStyle.BoldText]}>Add Store</Text>
+        </TouchableOpacity>
       </View>
       <RecentStores />
     </SafeAreaView>
