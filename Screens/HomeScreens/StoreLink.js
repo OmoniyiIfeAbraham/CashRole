10466f033462295ed89e596407ad797e4de9faca
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2024-05-24 05:57:34 +0100

    Update -- Working On StoreLink Screen

--- Screens/HomeScreens/StoreLink.js
+++ Screens/HomeScreens/StoreLink.js
@@ -1,7 +1,9 @@
-import { View, Text, Pressable, Keyboard } from "react-native";
+import { View, Text, Pressable, Keyboard, TextInput } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Colors from "../../Style/ThemeColors";
+import GeneralStyle from "../../Style/General.style";
+import { FontAwesome } from "@expo/vector-icons";
 
 const StoreLink = () => {
   return (
@@ -18,7 +20,36 @@ const StoreLink = () => {
             alignItems: "center",
             backgroundColor: Colors.midnightBlue,
           }}
-        ></View>
+        >
+          {/* search bar */}
+          <View
+            style={[
+              GeneralStyle.TextInputView,
+              {
+                width: "50%",
+                height: 40,
+                backgroundColor: Colors.ash,
+                borderWidth: 1,
+                borderColor: Colors.white,
+                borderRadius: 5,
+                marginBottom: 0,
+                paddingHorizontal: 4,
+              },
+            ]}
+          >
+            <FontAwesome
+              name="search"
+              size={24}
+              color={Colors.black}
+              style={{ marginRight: 10 }}
+            />
+            <TextInput
+              style={GeneralStyle.TextInput}
+              placeholder="Search for product"
+            />
+          </View>
+          {/* phone icon */}
+        </View>
       </Pressable>
     </SafeAreaView>
   );
