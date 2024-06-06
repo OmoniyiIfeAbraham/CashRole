39bf776c3b94405ea7d2674df58567813a73be4e
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2024-06-06 23:00:23 +0100

    Working On Image Upload

--- Components/Products/ImageUploader.js
+++ Components/Products/ImageUploader.js
@@ -1,12 +1,127 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useState, useEffect } from "react";
+import {
+  TouchableOpacity,
+  Image,
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import Colors from "../../Style/ThemeColors";
+import { FontAwesome6 } from "@expo/vector-icons";
+import GeneralStyle from "../../Style/General.style";
+import { ALERT_TYPE, Toast } from "react-native-alert-notification";
 
-const ImageUploader = () => {
+export default function ImageUploader({ width, height }) {
+  const [images, setImages] = useState([]);
+
+  const pickImage = async () => {
+    setImages([]);
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      aspect: [4, 3],
+      quality: 1,
+      allowsMultipleSelection: true,
+    });
+
+    if (!result.canceled) {
+      const newUris = result.assets.map((asset) => asset.uri);
+      setImages((prevImages) => {
+        const updatedImages = [...prevImages, ...newUris];
+        console.log(updatedImages); // Log the updated images array here
+        return updatedImages;
+      });
+    }
+    if (result.assets.length > 10) {
+      setImages([]);
+      Toast.show({
+        type: ALERT_TYPE.WARNING,
+        title: "You can Only Add 10 Images Per-Product!!!",
+      });
+    } else {
+      Toast.show({
+        type: ALERT_TYPE.SUCCESS,
+        title: `${result.assets.length} Image(s) Added Successfully`,
+      });
+    }
+  };
+
+  // Use useEffect to log the images array whenever it changes
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
   return (
-    <View>
-      <Text>ImageUploader</Text>
+    <View style={{ width: "100%", height: "auto", alignItems: "center" }}>
+      {/* title */}
+      <Text style={[GeneralStyle.RegularText, { color: Colors.black }]}>
+        Upload Photo(s)
+      </Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: Colors.white,
+          elevation: 5,
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 10,
+          padding: 25,
+          marginTop: 5,
+        }}
+        onPress={pickImage}
+      >
+        <FontAwesome6 name="add" size={24} color={Colors.black} />
+      </TouchableOpacity>
+      {images.length > 0 && (
+        <ScrollView
+          style={{
+            width: width,
+            maxHeight: 350,
+            marginTop: 10,
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <View
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {images.map((uri, index) => (
+              <View
+                key={index}
+                style={{
+                  width: "48%",
+                  height: 150,
+                  backgroundColor: "blue",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginBottom: 10,
+                }}
+              >
+                <Image
+                  source={{ uri }}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                  }}
+                />
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+      )}
     </View>
-  )
+  );
 }
 
-export default ImageUploader
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+    margin: 5,
+  },
+});
--- Screens/Sellers Screens/Stores/Products/AddProduct.js
+++ Screens/Sellers Screens/Stores/Products/AddProduct.js
@@ -5,72 +5,53 @@ import {
   Keyboard,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../../../Components/Header/Header";
 import GeneralStyle from "../../../../Style/General.style";
 import Colors from "../../../../Style/ThemeColors";
-import { FontAwesome6 } from "@expo/vector-icons";
+import ImageUploader from "../../../../Components/Products/ImageUploader";
 
 const AddProduct = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1, paddingHorizontal: 15 }}>
       {/* keyboard dismiss */}
-      <Pressable style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
+      <Pressable
+        style={{ flex: 1, width: "100%", height: "100%" }}
+        onPress={() => Keyboard.dismiss()}
+      >
         {/* header */}
         <Header navigation={navigation} title="Add new product" />
         {/* form */}
-        {/* title */}
-        <Text
-          style={[
-            GeneralStyle.RegularText,
-            { color: Colors.black, marginBottom: 5, marginTop: 25 },
-          ]}
-        >
-          Product name
-        </Text>
-        <View style={[GeneralStyle.TextInputView, { width: "100%" }]}>
-          <TextInput
-            style={GeneralStyle.TextInput}
-            placeholder="Enter name of product here"
-            placeholderTextColor={Colors.ash}
-            autoCapitalize="none"
-            keyboardType="default"
-          />
-        </View>
-        {/* Upload Photo */}
-        <View
-          style={{
-            width: "100%",
-            height: "auto",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={{ paddingBottom: 20 }}
         >
           {/* title */}
           <Text
             style={[
               GeneralStyle.RegularText,
-              { color: Colors.black, marginTop: 50 },
+              { color: Colors.black, marginBottom: 5, marginTop: 25 },
             ]}
           >
-            Upload Photo(s)
+            Product name
           </Text>
-          <TouchableOpacity
-            style={{
-              backgroundColor: Colors.white,
-              elevation: 5,
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 10,
-              padding: 25,
-              marginTop: 5,
-            }}
-          >
-            <FontAwesome6 name="add" size={24} color={Colors.black} />
-          </TouchableOpacity>
-        </View>
+          <View style={[GeneralStyle.TextInputView, { width: "100%" }]}>
+            <TextInput
+              style={GeneralStyle.TextInput}
+              placeholder="Enter name of product here"
+              placeholderTextColor={Colors.ash}
+              autoCapitalize="none"
+              keyboardType="default"
+            />
+          </View>
+          {/* Upload Photo */}
+          <ImageUploader width="100%" height="60%" />
+          {/* Other components can go here */}
+          <Text>zdxgfhcgjwervbk</Text>
+        </ScrollView>
       </Pressable>
     </SafeAreaView>
   );
