ebe6a21547e3f1b9a359a123d5c23b73a3a613dc
Ife Abraham <ife04abraham@gmail.com>
2025-07-07 13:22:40 +0100

    updated base api url

--- Global/Global.js
+++ Global/Global.js
@@ -1,6 +1,6 @@
 // export const baseAPIUrl = "https://staging.api.cashrole.com";
-export const baseAPIUrl = "http://10.112.236.229:4000";
-// export const baseAPIUrl = "https://cashrole-api.onrender.com";
+// export const baseAPIUrl = "http://10.112.236.229:4000";
+export const baseAPIUrl = "https://cashrole-api.onrender.com";
 export const ApiKey = "0U25eCA2goCf3AoY";
 export const ApiSecKey =
   "PT5uWu.k4s95dwENZZEKiz2ZGRqVUmQml9IpIWq.AOnr7gBWiWMVqXYj";
